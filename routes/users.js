f0e6480021fe99fc6e6f683237424c95544972b4
Nadee95 <nadeekacsuop@gmail.com>
2020-02-22 06:53:37 +0530

    ok

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,7 @@ const passport = require("passport");
 const jwt = require("jsonwebtoken");
 const { registerValidation, loginValidation } = require("../validation");
 
-const verify = require("./routes/verifyToken");
+//const verify = require("./routes/verifyToken");
 
 const User = require("../models/user");
 const config = require("../config/database");
